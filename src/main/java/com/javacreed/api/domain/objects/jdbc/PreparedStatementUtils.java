b9d64f8a6615f3db4949d875c810f42bae2d90be
Albert Attard <albert@javacreed.com>
2017-10-11 17:51:25 +0200

    Added support to Optional

--- src/main/java/com/javacreed/api/domain/objects/jdbc/PreparedStatementUtils.java
+++ src/main/java/com/javacreed/api/domain/objects/jdbc/PreparedStatementUtils.java
@@ -3,6 +3,7 @@ package com.javacreed.api.domain.objects.jdbc;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.util.Optional;
 
 import com.javacreed.api.domain.objects.LongBasedDomainObject;
 import com.javacreed.api.domain.objects.StringBasedDomainObject;
@@ -10,7 +11,7 @@ import com.javacreed.api.domain.objects.StringBasedDomainObject;
 public class PreparedStatementUtils {
 
   public static void setLong(final PreparedStatement ps, final int column, final LongBasedDomainObject object)
-      throws SQLException {
+      throws SQLException, NullPointerException {
     if (object != null) {
       ps.setLong(column, object.getValue());
     } else {
@@ -18,8 +19,17 @@ public class PreparedStatementUtils {
     }
   }
 
+  public static void setString(final PreparedStatement ps, final int column,
+      final Optional<? extends StringBasedDomainObject> object) throws SQLException, NullPointerException {
+    if (object.isPresent()) {
+      PreparedStatementUtils.setString(ps, column, object.get());
+    } else {
+      ps.setNull(column, Types.VARCHAR);
+    }
+  }
+
   public static void setString(final PreparedStatement ps, final int column, final StringBasedDomainObject object)
-      throws SQLException {
+      throws SQLException, NullPointerException {
     if (object != null && object.getValue().isPresent()) {
       ps.setString(column, object.getValue().get());
     } else {
@@ -28,5 +38,4 @@ public class PreparedStatementUtils {
   }
 
   private PreparedStatementUtils() {}
-
 }
