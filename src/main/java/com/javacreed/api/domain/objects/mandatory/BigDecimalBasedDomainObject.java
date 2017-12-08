f6a05a55f292cf82513f584a74718c51ef631e98
Albert Attard <albert@javacreed.com>
2017-12-08 22:55:23 +0100

    Added BigDecimal base objects

---
+++ src/main/java/com/javacreed/api/domain/objects/mandatory/BigDecimalBasedDomainObject.java
@@ -0,0 +1,33 @@
+package com.javacreed.api.domain.objects.mandatory;
+
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+
+import javax.annotation.concurrent.Immutable;
+
+import com.google.common.base.Preconditions;
+
+@Immutable
+public class BigDecimalBasedDomainObject extends ObjectBasedDomainObject<BigDecimal>
+    implements Comparable<BigDecimalBasedDomainObject> {
+
+  private static final NumberFormat DEFAULT_FORMAT = NumberFormat.getNumberInstance();
+
+  protected BigDecimalBasedDomainObject(final BigDecimal value) throws NullPointerException {
+    super(value);
+  }
+
+  @Override
+  public int compareTo(final BigDecimalBasedDomainObject other) {
+    return compareTo(other, BigDecimal::compareTo);
+  }
+
+  public String format() {
+    return format(BigDecimalBasedDomainObject.DEFAULT_FORMAT);
+  }
+
+  public String format(final NumberFormat format) throws NullPointerException {
+    Preconditions.checkNotNull(format);
+    return map(format::format);
+  }
+}
