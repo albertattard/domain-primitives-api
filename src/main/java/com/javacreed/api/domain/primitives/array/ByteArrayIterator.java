674325d616ba0bdb92ecbd3d1939d08e680b9570
Alber Attard <albert@javacreed.com>
2018-12-05 17:34:25 +0100

    Added comments and removed runtime exceptions to improve the code readability

--- src/main/java/com/javacreed/api/domain/primitives/array/ByteArrayIterator.java
+++ src/main/java/com/javacreed/api/domain/primitives/array/ByteArrayIterator.java
@@ -7,10 +7,26 @@ import javax.annotation.concurrent.NotThreadSafe;
 
 import com.google.common.base.Preconditions;
 
+/**
+ * A basic implementation of an iterator that supports primitive values
+ *
+ * @author Albert Attard
+ */
 @NotThreadSafe
 public class ByteArrayIterator implements Iterator<Byte> {
 
-  public static ByteArrayIterator create(final byte[] data) throws NullPointerException {
+  /**
+   * Creates an iterator based on the given array. Note that the given array is not copied, thus any changes to the
+   * array's content are shared by the iterator. This iterator does not modify the array's content and does not support
+   * the {@link #remove()} operation.
+   *
+   * @param data
+   *          the array based on which the iterator is created (which cannot be {@code null})
+   * @return an instance of the iterator
+   * @throws NullPointerException
+   *           if the given parameter is {@code null}
+   */
+  public static ByteArrayIterator create(final byte[] data) {
     Preconditions.checkNotNull(data);
     return new ByteArrayIterator(data);
   }
@@ -28,11 +44,27 @@ public class ByteArrayIterator implements Iterator<Byte> {
   }
 
   @Override
-  public Byte next() throws NoSuchElementException {
+  public Byte next() {
     return nextByte();
   }
 
-  public byte nextByte() throws NoSuchElementException {
+  /**
+   * Returns the next byte value if one is available, otherwise throws a {@link NoSuchElementException}. This method
+   * should be called after first verifying the iterator's availability through the {@link #hasNext()} method.
+   *
+   * <pre>
+   * final ByteArrayIterator iterator = ByteArrayIterator.create(new byte[] { 1, 2, 3, 4 });
+   * while (iterator.hasNext()) {
+   *   final byte b = nextByte();
+   * }
+   * </pre>
+   *
+   * @return the next byte value
+   * @throws NoSuchElementException
+   *           if no more elements are available
+   * @see {@link #hasNext()}
+   */
+  public byte nextByte() {
     if (index == data.length) {
       throw new NoSuchElementException("No more elements are available");
     }
