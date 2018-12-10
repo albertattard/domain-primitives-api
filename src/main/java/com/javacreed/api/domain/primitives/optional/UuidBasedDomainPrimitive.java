2f9f72e54d84610339901129122f6c18732c854f
Alber Attard <albert@javacreed.com>
2018-12-10 17:08:24 +0100

    Added new test

--- src/main/java/com/javacreed/api/domain/primitives/optional/UuidBasedDomainPrimitive.java
+++ src/main/java/com/javacreed/api/domain/primitives/optional/UuidBasedDomainPrimitive.java
@@ -1,10 +1,12 @@
 package com.javacreed.api.domain.primitives.optional;
 
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.UUID;
 
 import javax.annotation.concurrent.Immutable;
 
+import com.google.common.base.Preconditions;
 import com.javacreed.api.domain.primitives.utils.UuidUtils;
 
 @Immutable
@@ -34,7 +36,7 @@ public class UuidBasedDomainPrimitive extends ComparableBasedDomainPrimitive<UUI
    * class.
    *
    * @return the converted (incompatible with the {@link UUID#nameUUIDFromBytes()}) bytes if available, otherwise
-   *         <code>null</code>
+   *         {@code null}
    * @see #toBytes()
    */
   public byte[] toBytesOrNull() {
@@ -47,5 +49,21 @@ public class UuidBasedDomainPrimitive extends ComparableBasedDomainPrimitive<UUI
 
   public String toFormattedStringOrNull() {
     return toFormattedString().orElse(null);
+  }
+
+  /**
+   * Compares the given byte array to the byte equivalent value of this UUID ({@link #toBytes()}) and returns
+   * {@code true}, if the value is set and the given byte array has the same content as this UUID byte value,
+   * {@code false} otherwise.
+   * 
+   * @param other
+   *          the array to compare to (which cannot be {@code null})
+   * @return {@code true} if the given array has the same byte value of this UUID, {@code false} otherwise
+   * @throws NullPointerException
+   *           if the given parameter is {@code null}
+   */
+  public boolean sameValue(byte[] other) {
+    Preconditions.checkNotNull(other);
+    return toBytes().map(b -> Arrays.equals(b, other)).orElse(false);
   }
 }
