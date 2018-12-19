1d061b8caef51208fa41c111174ae2c38dcfbe7e
Alber Attard <albert@javacreed.com>
2018-12-19 22:59:21 +0100

    Made the object a supplier and deprecated a duplicate method

--- src/main/java/com/javacreed/api/domain/primitives/mandatory/ObjectBasedDomainPrimitive.java
+++ src/main/java/com/javacreed/api/domain/primitives/mandatory/ObjectBasedDomainPrimitive.java
@@ -3,6 +3,7 @@ package com.javacreed.api.domain.primitives.mandatory;
 import java.util.Comparator;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 import javax.annotation.concurrent.Immutable;
 
@@ -18,7 +19,7 @@ import com.google.common.base.Preconditions;
  *          the type of the value that is contained by this domain primitive
  */
 @Immutable
-public class ObjectBasedDomainPrimitive<T> {
+public class ObjectBasedDomainPrimitive<T> implements Supplier<T> {
 
   /** The raw value wrapped within this domain primitive */
   protected final T value;
@@ -48,7 +49,7 @@ public class ObjectBasedDomainPrimitive<T> {
    *           if the given parameters are {@code null}
    */
   protected int compareTo(final ObjectBasedDomainPrimitive<T> other, final Comparator<T> comparator) {
-    return comparator.compare(getValue(), other.getValue());
+    return comparator.compare(get(), other.get());
   }
 
   @Override
@@ -63,26 +64,33 @@ public class ObjectBasedDomainPrimitive<T> {
 
     @SuppressWarnings("rawtypes")
     final ObjectBasedDomainPrimitive other = (ObjectBasedDomainPrimitive) object;
-    return value.equals(other.value);
+    return get().equals(other.get());
+  }
+
+  @Override
+  public T get() {
+    return value;
   }
 
   /**
    * Returns the value contained by the primitive object, which value is never {@code null}
    *
    * @return the value contained by the primitive object, which value is never {@code null}
+   * @deprecated use {@link #get()} instead
    */
+  @Deprecated
   public T getValue() {
-    return value;
+    return get();
   }
 
   @Override
   public int hashCode() {
-    return value.hashCode();
+    return get().hashCode();
   }
 
   /**
-   * Convert the value using the given {@code mapper}. The value returned could be anything, including {@code null}, as
-   * this is determined by the {@code mapper} function.
+   * Convert the value using the given <code> mapper</code>. The value returned could be anything, including
+   * <code>null</code>, as this is determined by the <code>mapper</code> function.
    *
    * @param <U>
    *          The type of the result of the mapping function
@@ -95,11 +103,36 @@ public class ObjectBasedDomainPrimitive<T> {
    */
   public <U> U map(final Function<? super T, ? extends U> mapper) {
     Preconditions.checkNotNull(mapper);
-    return mapper.apply(value);
+    return mapper.apply(get());
+  }
+
+  /**
+   * Returns <code>true</code> if the value returned by the given <code>supplier</code> is equal to the value of this
+   * primitive, <code>false</code> otherwise.
+   *
+   * @param supplier
+   *          supplier of the value that will be compared (which can be <code>null</code> and can return a
+   *          <code>null</code> value)
+   * @return <code>true</code> if the value returned by the given <code>supplier</code> is equal to the value of this
+   *         primitive, <code>false</code> otherwise
+   */
+  public boolean sameValue(final Supplier<T> supplier) {
+    return supplier != null && sameValue(supplier.get());
+  }
+
+  /**
+   * Returns <code>true</code> if the given object is equal to the value of this primitive, <code>false</code> otherwise
+   *
+   * @param other
+   *          the value to compare against (which can be <code>null</code>)
+   * @return <code>true</code> if the given object is equal to the value of this primitive, <code>false</code> otherwise
+   */
+  public boolean sameValue(final T other) {
+    return other != null && get().equals(other);
   }
 
   @Override
   public String toString() {
-    return String.valueOf(value);
+    return String.valueOf(get());
   }
 }
