a7dc43f4a2adfb6ea96e72559765d3da9d102b6e
Albert Attard <albert.attard@thoughtworks.com>
2019-12-23 20:40:18 +0100

    Improve test coverage

--- src/test/java/com/javacreed/api/domain/primitives/utils/ComparatorUtilsTest.java
+++ src/test/java/com/javacreed/api/domain/primitives/utils/ComparatorUtilsTest.java
@@ -6,7 +6,14 @@ import org.junit.Test;
 public class ComparatorUtilsTest {
 
   @Test
-  public void compareValues() {
+  public void should_safely_compare_null_values() {
+    Assert.assertEquals(0, ComparatorUtils.compare(null, null, String.CASE_INSENSITIVE_ORDER));
+    Assert.assertEquals(-1, ComparatorUtils.compare(null, "a", String.CASE_INSENSITIVE_ORDER));
+    Assert.assertEquals(1, ComparatorUtils.compare("a", null, String.CASE_INSENSITIVE_ORDER));
+  }
+
+  @Test
+  public void should_sort_values_using_the_given_comparator() {
     Assert.assertEquals(0, ComparatorUtils.compare("a", "A", String.CASE_INSENSITIVE_ORDER));
     Assert.assertEquals(0, ComparatorUtils.compare("A", "a", String.CASE_INSENSITIVE_ORDER));
     Assert.assertEquals(-1, ComparatorUtils.compare("a", "B", String.CASE_INSENSITIVE_ORDER));
@@ -16,14 +23,7 @@ public class ComparatorUtilsTest {
   }
 
   @Test
-  public void handleNulls() {
+  public void should_throw_an_NullPointerException_when_given_null_comparator() {
     Assert.assertThrows(NullPointerException.class, () -> ComparatorUtils.compare("a", "A", null));
-  }
-
-  @Test
-  public void handleNullValues() {
-    Assert.assertEquals(0, ComparatorUtils.compare(null, null, String.CASE_INSENSITIVE_ORDER));
-    Assert.assertEquals(-1, ComparatorUtils.compare(null, "a", String.CASE_INSENSITIVE_ORDER));
-    Assert.assertEquals(1, ComparatorUtils.compare("a", null, String.CASE_INSENSITIVE_ORDER));
   }
 }
--- src/test/java/com/javacreed/api/domain/primitives/utils/UuidUtilsTest.java
+++ src/test/java/com/javacreed/api/domain/primitives/utils/UuidUtilsTest.java
@@ -7,23 +7,9 @@ import org.junit.Test;
 
 public class UuidUtilsTest {
 
-  @Test
-  public void asUuid() {
-    Assert.assertThrows(NullPointerException.class, () -> UuidUtils.asUuid(null));
-    Assert.assertThrows(IllegalArgumentException.class, () -> UuidUtils.asUuid(new byte[10]));
-  }
-
-  @Test
-  public void checkArgument() {
-    final String uuid = "92f53821-01c5-4020-85c7-86cc2f0211ff";
-    Assert.assertEquals(uuid, UuidUtils.checkArgument(uuid));
-
-    Assert.assertThrows(NullPointerException.class, () -> UuidUtils.checkArgument(null));
-  }
-
   /**
-   * The UUID utils is not compatible with the {@link UUID#nameUUIDFromBytes()} method. Verifies the incompatibility
-   * between these two
+   * The UUID utils is not compatible with the {@link UUID#nameUUIDFromBytes(byte[])}} method. Verifies the
+   * incompatibility between these two
    *
    * @see UUID#nameUUIDFromBytes(byte[])
    */
@@ -34,8 +20,24 @@ public class UuidUtilsTest {
     Assert.assertNotEquals(expected, incompatible);
   }
 
+  /**
+   * Verifies that whatever is converted to bytes can be converted back to the same UUID
+   */
   @Test
-  public void isValid() {
+  public void should_create_the_same_UUID() {
+    final UUID expected = UUID.randomUUID();
+    final UUID actual = UuidUtils.asUuid(UuidUtils.asBytes(expected));
+    Assert.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void should_return_the_given_UUID() {
+    final String uuid = "92f53821-01c5-4020-85c7-86cc2f0211ff";
+    Assert.assertEquals(uuid, UuidUtils.checkArgument(uuid));
+  }
+
+  @Test
+  public void should_return_true_when_given_a_value_UUID_otherwise_false() {
     Assert.assertTrue(UuidUtils.isValid(UUID.randomUUID().toString()));
 
     Assert.assertFalse(UuidUtils.isValid(null));
@@ -47,13 +49,17 @@ public class UuidUtilsTest {
     Assert.assertFalse(UuidUtils.isValid("01234567-89ab-cdef-012-456789abcdef0"));
   }
 
-  /**
-   * Verifies that whatever is converted to bytes can be converted back to the same UUID
-   */
   @Test
-  public void readWrite() {
-    final UUID expected = UUID.randomUUID();
-    final UUID actual = UuidUtils.asUuid(UuidUtils.asBytes(expected));
-    Assert.assertEquals(expected, actual);
+  public void should_throw_a_NullPointerException_when_given_null() {
+    Assert.assertThrows(NullPointerException.class, () -> UuidUtils.asUuid(null));
+    Assert.assertThrows(NullPointerException.class, () -> UuidUtils.checkArgument(null));
+  }
+
+  @Test
+  public void should_throw_an_IllegalArgumentException_when_given_an_invalid_input() {
+    Assert.assertThrows(IllegalArgumentException.class, () -> UuidUtils.asUuid(new byte[10]));
+
+    Assert.assertThrows(IllegalArgumentException.class, () -> UuidUtils.checkArgument(""));
+    Assert.assertThrows(IllegalArgumentException.class, () -> UuidUtils.checkArgument("123456789_123456789_123456789_123456"));
   }
 }
