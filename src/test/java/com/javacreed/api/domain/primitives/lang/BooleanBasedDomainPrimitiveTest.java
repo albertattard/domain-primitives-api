99e4273dff0ce31a10848cb297f6fbe9d32a8e24
Albert Attard <albert.attard@thoughtworks.com>
2019-12-23 19:42:38 +0100

    Improve test coverage

--- src/test/java/com/javacreed/api/domain/primitives/lang/BooleanBasedDomainPrimitiveTest.java
+++ src/test/java/com/javacreed/api/domain/primitives/lang/BooleanBasedDomainPrimitiveTest.java
@@ -5,13 +5,35 @@ import org.junit.Test;
 
 public class BooleanBasedDomainPrimitiveTest {
 
+  private static class Subject extends BooleanBasedDomainPrimitive {
+    private Subject(final boolean value) {
+      super(value);
+    }
+  }
+
   @Test
-  public void trueIfNotNullAndPositive() {
+  public void should_return_1_when_true_0_when_false() {
+    Assert.assertEquals(0, new Subject(false).asByte());
+    Assert.assertEquals(1, new Subject(true).asByte());
+  }
+
+  @Test
+  public void should_return_true_if_not_null_and_positive_false_otherwise() {
     Assert.assertFalse(BooleanBasedDomainPrimitive.trueIfNotNullAndPositive(null));
     Assert.assertFalse(BooleanBasedDomainPrimitive.trueIfNotNullAndPositive(Byte.valueOf((byte) 0)));
     Assert.assertFalse(BooleanBasedDomainPrimitive.trueIfNotNullAndPositive(Byte.MIN_VALUE));
 
     Assert.assertTrue(BooleanBasedDomainPrimitive.trueIfNotNullAndPositive(Byte.valueOf((byte) 1)));
     Assert.assertTrue(BooleanBasedDomainPrimitive.trueIfNotNullAndPositive(Byte.MAX_VALUE));
+  }
+
+  @Test
+  public void should_sort_in_descending_order() {
+    final Subject a = new Subject(false);
+    final Subject b = new Subject(true);
+
+    Assert.assertEquals(0, BooleanBasedDomainPrimitive.DESCENDING_ORDER.compare(a, a));
+    Assert.assertEquals(1, BooleanBasedDomainPrimitive.DESCENDING_ORDER.compare(a, b));
+    Assert.assertEquals(-1, BooleanBasedDomainPrimitive.DESCENDING_ORDER.compare(b, a));
   }
 }
